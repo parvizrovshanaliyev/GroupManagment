e145b6099772b2daee2b324fa07b680eb4fdef27
parvizrovshanaliyev <parvizra@code.edu.az>
2020-03-29 20:45:15 +0400

    nlog added

--- src/PlayBall.GroupManagement.Web/Program.cs
+++ src/PlayBall.GroupManagement.Web/Program.cs
@@ -1,12 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
+using LogLevel = NLog.LogLevel;
 
 namespace PlayBall.GroupManagement.Web
 {
@@ -15,18 +16,50 @@ namespace PlayBall.GroupManagement.Web
         public static void Main(string[] args)
         {
             Console.WriteLine("#######starting app########");
-
+            ConfigureNLog();
             CreateHostBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .Build()
                 .Run();
         }
+
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureLogging(logging =>
+                {
+                    // clear default providers, nlog handle it
+                    logging.ClearProviders();
+
+                    // set minimum level to trace, nlog rules will kick in afterwards
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: setup NLog for Dependency injection;
+
+        // TODO: replace with nlog.config
+        private static void ConfigureNLog()
+        {
+            var config =
+                new LoggingConfiguration();
+
+            var consoleTarget =
+                 new ColoredConsoleTarget("coloredConsole")
+                 {
+                     Layout = @"${date:format=HH\:mm\:ss} ${level} ${message} ${exception}"
+                 };
+
+            config.AddTarget(consoleTarget);
+
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget, "PlayBall.GroupManagement.Web.IoC.*");
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, consoleTarget);
+
+            LogManager.Configuration = config;
+        }
     }
+
 }
