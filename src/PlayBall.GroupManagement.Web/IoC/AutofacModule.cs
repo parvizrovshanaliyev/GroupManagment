e233865041ed6c279a1b10c8eb128148b69c1245
parvizrovshanaliyev <parvizra@code.edu.az>
2020-03-29 20:07:21 +0400

    starting nlog provider

--- src/PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
+++ src/PlayBall.GroupManagement.Web/IoC/AutofacModule.cs
@@ -1,59 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Autofac;
+using Microsoft.Extensions.Logging;
 using PlayBall.GroupManagement.Business.Implement.Services;
 using PlayBall.GroupManagement.Business.Model;
 using PlayBall.GroupManagement.Business.Services;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace PlayBall.GroupManagement.Web.IoC
 {
     public class AutoFacModule : Module
 
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<InMemoryGroupService>()
-                .Named<IGroupService>("groupService")
-                .SingleInstance();
-            builder.RegisterDecorator<IGroupService>((context,service)=>
-                new GroupServiceDecorator(service),"groupService" );
+            builder.RegisterType<InMemoryGroupService>().Named<IGroupService>("groupService").SingleInstance();
+            builder.RegisterDecorator<IGroupService>(
+                (context, service) =>
+                    new GroupServiceDecorator(service, context.Resolve<ILogger<GroupServiceDecorator>>()),
+                "groupService");
         }
 
 
         private class GroupServiceDecorator : IGroupService
         {
             private readonly IGroupService _inner;
+            private ILogger<GroupServiceDecorator> _logger;
 
-            public GroupServiceDecorator(IGroupService inner)
+            public GroupServiceDecorator(IGroupService inner,
+                ILogger<GroupServiceDecorator> logger)
             {
                 _inner = inner;
+                _logger = logger;
             }
 
             public IReadOnlyCollection<Group> GetAll()
             {
-                Console.WriteLine($"######## Hello from {nameof(GetAll)} ######");
-                return _inner.GetAll();
+                using (var scope =
+                    _logger.BeginScope("Decorator scope: {decorator}",
+                        nameof(GroupServiceDecorator)))
+                {
+                    _logger.LogTrace("######## Hello from {decoratedMethod} ######",
+                        nameof(GetAll));
+
+                    var result =
+                        _inner.GetAll();
+
+                    _logger.LogTrace("######## Goodbye from {decoratedMethod} ######",
+                        nameof(GetAll));
+
+                    return result;
+                }
             }
 
             public Group GetById(long id)
             {
-                Console.WriteLine($"######## Hello from {nameof(GetById)} ######");
+                _logger.LogTrace("######## Hello from {decoratedMethod} ######", nameof(GetById));
 
                 return _inner.GetById(id);
             }
 
             public Group Update(Group group)
             {
-                Console.WriteLine($"######## Hello from {nameof(Update)} ######");
+                _logger.LogTrace("######## Hello from {decoratedMethod} ######", nameof(Update));
 
                 return _inner.Update(group);
             }
 
             public Group Add(Group group)
             {
-                Console.WriteLine($"######## Hello from {nameof(Add)} ######");
+                _logger.LogTrace("######## Hello from {decoratedMethod} ######", nameof(Add));
 
-                return _inner.Add(group);
+                return _inner.Add(group);  
             }
         }
     }
